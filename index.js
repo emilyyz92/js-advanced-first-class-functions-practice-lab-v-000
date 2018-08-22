b52bf864f6b5a23565979f93aa2e05e6277d54e2
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-22 18:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ const driversByRevenue = function (driversArray) {
 
 const driversByName = function (driversArray) {
   const newDriversArray = [...driversArray];
-  return newDriversArray.sort( function(a,b) { return a.localecomapre(b)})
+  return newDriversArray.sort( function(a,b) { return a["name"].localecomapre(b["name"])})
 }
