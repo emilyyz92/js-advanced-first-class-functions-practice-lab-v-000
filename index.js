e36ba1474b59a6a5369ccf57e73edc75b4e6ce73
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-22 15:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const logDriverNames = function (driversArray) {
   driversArray.forEach(function (el, i, array) {
-    console.log(el);
+    console.log(el["name"]);
   })
 }
